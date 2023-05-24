060a8102b2e39c009694764f196c49b5fcc517ea
laycookie <80195126+laycookie@users.noreply.github.com>
2023-05-24 15:26:15 -0700

    UI update

---
+++ test/web/src/main.d.ts
@@ -0,0 +1,10 @@
+type Settings = {
+    zoom: number,
+    ini_y: number,
+    ini_angle: number,
+  
+    circleAmountX: number,
+    circleAmountY: number,
+    circleRadius: number,
+    circleDistance: number,
+  }
--- test/web/src/main.ts
+++ test/web/src/main.ts
@@ -1,69 +1,28 @@
-import { simulate } from "chaos_theory";
-import * as PIXI from "pixi.js";
+import {Settings} from './main.d.ts'
+import { render } from './renderer'
 
-const res_x = 500;
-const res_y = 500;
-let app = new PIXI.Application({
-  width: res_x,
-  height: res_y,
-  antialias: true,
-});
-document.body.appendChild(app.view as unknown as Node);
+let settings: Settings = {
+  zoom: 30,
+  ini_y: 0,
+  ini_angle: 20,
 
-const SCALER_CONST = 30;
-
-function calculateNewPoint(originalX: number, originalY: number, angleInDegrees: number, distance: number) {
-  // Convert angle from degrees to radians
-  let angleInRadians = angleInDegrees * (Math.PI / 180);
-
-  // Calculate the new coordinates
-  let newX = (distance * Math.cos(angleInRadians));
-  let newY =  (distance * Math.sin(angleInRadians));
-
-  // Return the new coordinates as an object
-  return { x: newX, y: newY };
+  circleAmountX: 3,
+  circleAmountY: 3,
+  circleRadius: 1,
+  circleDistance: 3,
 }
 
-const out = JSON.parse(simulate(3, 3, 3, 1, 0, 0, 50));
-// create circle sprite
-for (let i of out.circles) {
-  let circle = new PIXI.Graphics();
-  circle.beginFill(0x9966ff);
-  circle.drawCircle(
-    i.x * SCALER_CONST,
-    -i.y * SCALER_CONST,
-    i.radius * SCALER_CONST
-  );
-  circle.endFill();
-  circle.x = res_x / 2;
-  circle.y = res_y / 2;
-  app.stage.addChild(circle);
-}
-// create laser beams
-for (let i of out.laser_beams) {
-  if (!i.bounces) {
-    let line = new PIXI.Graphics();
-    line.lineStyle(1, 0xffffff);
-    line.moveTo(i.x * SCALER_CONST, -i.y * SCALER_CONST);
+const formSubmition = document.getElementById('form-submition') as HTMLFormElement;
+formSubmition.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-    const line_end = calculateNewPoint(i.x, i.y, i.angle, 100);
-    line.lineTo(line_end.x * SCALER_CONST, -line_end.y * SCALER_CONST);
-
-    line.x = res_x / 2;
-    line.y = res_y / 2;
-    app.stage.addChild(line);
-
-    console.log(line_end);
-    console.log(i);
-    break;
+  const formData = new FormData(formSubmition);
+  let angle = formData.get("angle");
+  if (angle == "") {
+    angle = "0";
   }
+  settings.ini_angle = Number(angle);
+  render(settings);
+});
 
-  let line = new PIXI.Graphics();
-  line.lineStyle(1, 0xffffff);
-  line.moveTo(i.x * SCALER_CONST, -i.y * SCALER_CONST);
-  line.lineTo(i.end_x * SCALER_CONST, -i.end_y * SCALER_CONST);
-  line.x = res_x / 2;
-  line.y = res_y / 2;
-  app.stage.addChild(line);
-}
-console.log(out);
+render(settings)
