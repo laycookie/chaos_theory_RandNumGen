6a4c4670a04030822835101104af5a4abacdca1e
laycookie <80195126+laycookie@users.noreply.github.com>
2023-06-02 07:45:46 -0700

    Temp GUI

---
+++ test/next/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { manny_circle_set } from "chaos_theory";
+
+export default function Home() {
+  manny_circle_set(1, 1, 1, 1);
+
+  return (
+    <main>
+      <h1>Test</h1>
+    </main>
+  );
+}
